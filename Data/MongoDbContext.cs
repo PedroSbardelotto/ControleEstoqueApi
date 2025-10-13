f0c72e709cdd0a2648662f2b293d554e4d027628
Pedro Sbardelotto <sbardelottopedro97@gmail.com>
2025-10-13 09:04:59 -0300

    iniciando integracao com o front

--- Data/MongoDbContext.cs
+++ Data/MongoDbContext.cs
@@ -23,9 +23,9 @@ namespace ControleEstoque.Api.Data
             _database = client.GetDatabase(settings.Value.DatabaseName);
         }
 
-        public IMongoCollection<Cliente> Clientes => _database.GetCollection<Cliente>("Clientes");
-        public IMongoCollection<User> Usuarios => _database.GetCollection<User>("Usuarios");
-        public IMongoCollection<Produto> Produtos => _database.GetCollection<Produto>("Produtos");
-        public IMongoCollection<Pedido> Pedidos => _database.GetCollection<Pedido>("Pedidos");
+        public IMongoCollection<Cliente> Clientes => _database.GetCollection<Cliente>("Customer");
+        public IMongoCollection<User> Usuarios => _database.GetCollection<User>("User");
+        public IMongoCollection<Produto> Produtos => _database.GetCollection<Produto>("Products");
+        public IMongoCollection<Pedido> Pedidos => _database.GetCollection<Pedido>("Orders");
     }
 }
